8e1d55b39683f5ee3b56f2e19b4e98837f7c992b
DKFN <valere.tetelin@gmail.com>
2019-12-23 20:15:57 +0100

    Version 0.2, fixed damage setting working and fun

--- server/hit_handler.lua
+++ server/hit_handler.lua
@@ -1,13 +1,7 @@
-local hits = { }
-
-local BODY_POSITIONS = {
-	"HEAD",
-	"CORPSE",
-	"FEETS"
-}
-
 local OGK_TRUE_DMG_DEBUG = true
 local debug_npc
+local HEADSHOT_BONUS = 35
+local CORPSE_BONUS = 10
 
 local function debug(message)
 	if OGK_TRUE_DMG_DEBUG then
@@ -15,90 +9,52 @@ local function debug(message)
 	end
 end
 
-function OnPlayerWeaponShot(player, weapon, hittype, hitid, hitx, hity, hitz, startx, starty, startz, normalx, normaly, normalz)
+local function OnPlayerWeaponShot(player, weapon, hittype, hitid, hitx, hity, hitz, startx, starty, startz, normalx, normaly, normalz)
+	local healthSetter
+	local healthGetter
+	local positionGetter
+	
+	if hittype == 2 then
+		healthSetter = SetPlayerHealth
+		healthGetter = GetPlayerHealth
+		positionGetter = GetPlayerLocation
+	elseif hittype == 4 then
+		healthSetter = SetNPCHealth
+		healthGetter = GetNPCHealth
+		positionGetter = GetNPCLocation
+	end
+
 	if hittype == 2 or hittype == 4 then
     	-- First find the player that is in range of the hit
-		-- local victim = GetNearestPlayer2D(hitx, hity)
-		-- local victimx, victimy, victimz = GetPlayerLocation(victim)
-		
-		local victim = debug_npc;
-		local victimx, victimy, victimz = GetNPCLocation(debug_npc)
-
+		local victim = hitid
+		local victimx, victimy, victimz = positionGetter(hitid)
+	
 		local victim_feet_pos = victimz - 90;
 
-		-- Finding where the hit happend
+		-- Finding where the hit happend and then adding bonus/malus to dmg
 		local hit_pos
+		local victim_health = healthGetter(hitid)
+		local final_health = victim_health
+
 		if hitz > victim_feet_pos + 50 then
-			hit_pos = "CORPSE"
+			final_health = victim_health - CORPSE_BONUS
 		end
 		if hitz > victim_feet_pos + 150 then
-			hit_pos = "HEAD"
-		end
-
-		if not hit_pos then
-			hit_pos = "FEETS"
+			final_health = victim_health - HEADSHOT_BONUS
 		end
 		
-		-- We store it here as we are not sure that the player actually took damage with the position of the hit to see if
-		-- it is quite matching the position of when the player took the damage
-		hits[victim] = {
-			position = {hitx, hity, hitz},
-			instigator = player,
-			position = hit_pos
-		}
+		if final_health <= 0 then
+			final_health = 1
+		end
+
+		healthSetter(victim, final_health)
 		debug("Registered hit by "..player.." | VICTIM FEETS "..victim_feet_pos.." | HIT Y: "..hity.." HIT Z: "..hitz)
+		debug("Pre-hit damage setting"..final_health)
 	end
 end
 AddEvent("OnPlayerWeaponShot", OnPlayerWeaponShot)
 
--- For calculus of the damage boost it is simple
--- Head: Apply a boost on the damage (like 0.5)
--- Corpse: Do nothing
--- Feet: Add a bit to player health to counteract real damage
-function DamageHandler(player, damagetype, amount)
-	if hits[player] then
-		local hit = hits[player]
-		local hit_pos = hits[player].position
-		--local player_health = GetPlayerHealth(player)
-		local player_health = GetNPCHealth(player)
-		
-		debug("Player damage was registered. Caused by "..hit.instigator.." at position"..hit_pos)
-		
-		-- Defines the health to assign player before hit
-		local health_bonus = player_health
-		if hit_pos == "HEAD" then
-			health_bonus = -(amount * 0.5)
-		elseif hit_pos == "FEETS" then
-			health_bonus = (amount * 0.3)
-		else
-			health_bonus = 0
-		end
-
-		-- Check that the player still has 1 health at least after setting player health so that it is not the server
-		-- registering the kill
-		if player_health + health_bonus <= 0 then
-			health_bonus = 1
-		end
-
-		-- SetPlayerHealth(player, health_bonus)
-		SetNPCHealth(debug_npc, player_health + health_bonus)
-		
-		hits[player] = nil
-
-		AddPlayerChatAll("Bonus to health will be ".. health_bonus .. " bonus from hit")
-		
-		
-	else
-		debug("Player "..player.." did not took damage from registred gunshot")
-	end
-end
-AddEvent("OnPlayerDamage", DamageHandler)
-
-
 if OGK_TRUE_DMG_DEBUG then
-	AddEvent("OnNPCDamage", DamageHandler)
-
-	-- Debug function
 	AddEvent("OnPackageStart", function()
 		debug("Debug mode")
 		debug_npc = CreateNPC(42180.0, 201287.0, 551.0, 0)
